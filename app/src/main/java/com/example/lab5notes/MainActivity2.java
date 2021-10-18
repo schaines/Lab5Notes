bbe5fb1a49d005a0c11272f536c8923c520327c1
Sara Haines <schaines@wisc.edu>
2021-10-18 12:59:53 -0500

    finished milestone 2

--- app/src/main/java/com/example/lab5notes/MainActivity2.java
+++ app/src/main/java/com/example/lab5notes/MainActivity2.java
@@ -5,15 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
 
     TextView textView;
+    public static ArrayList<Note> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,29 @@ public class MainActivity2 extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences("com.example.lab5notes", Context.MODE_PRIVATE);
         String str = sharedPreferences.getString("username","");
         textView.setText("Welcome " + str + "!");
+
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("notes", Context.MODE_PRIVATE, null);
+        DBHelper dbHelper = new DBHelper(sqLiteDatabase);
+        notes = dbHelper.readNotes(str);
+
+        ArrayList<String> displayNotes = new ArrayList<>();
+        for (Note note:notes) {
+            displayNotes.add(String.format("Title:%s\nDate:%s", note.getTitle(), note.getDate()));
+        }
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, displayNotes);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity3.class);
+                intent.putExtra("noteid", i);
+                startActivity(intent);
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/lab5notes/MainActivity3.java
+++ app/src/main/java/com/example/lab5notes/MainActivity3.java
@@ -2,17 +2,64 @@ package com.example.lab5notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity3 extends AppCompatActivity {
 
+    int noteid = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        EditText editText = (EditText) findViewById(R.id.text);
+        Intent intent = getIntent();
+        noteid = intent.getIntExtra("noteid",-1);
+
+        if(noteid != -1) {
+            Note note = MainActivity2.notes.get(noteid);
+            String noteContent = note.getContent();
+            editText.setText(noteContent);
+        }
+
     }
 
     public void onClick(View view) {
+        EditText editText = (EditText) findViewById(R.id.text);
+        String content = editText.getText().toString();
+
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("notes", Context.MODE_PRIVATE, null);
+        DBHelper dbHelper = new DBHelper(sqLiteDatabase);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("com.example.lab5notes", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username","");
+
+        String title;
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        String date = dateFormat.format(new Date());
+
+        if (noteid == -1) {
+            title = "NOTE_" + (MainActivity2.notes.size() + 1);
+            dbHelper.saveNotes(username, title, content, date);
+        }
+        else {
+            title = "NOTE_" + (noteid+1);
+            dbHelper.updateNotes(title,date,content,username);
+        }
+
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("message", username);
+        startActivity(intent);
     }
 }
